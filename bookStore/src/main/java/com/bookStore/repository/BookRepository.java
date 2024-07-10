5294b26d1fc2d476e72fa6363703e356363954dc
pranav <vrk.com>
2024-07-10 20:34:07 +0530

    first commit

---
+++ bookStore/src/main/java/com/bookStore/repository/BookRepository.java
@@ -0,0 +1,13 @@
+package com.bookStore.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.bookStore.entity.Book;
+
+import java.util.List;
+
+@Repository
+public interface BookRepository extends JpaRepository<Book,Integer>  {
+    List<Book> findBynameContainingIgnoreCase(String name);
+}
